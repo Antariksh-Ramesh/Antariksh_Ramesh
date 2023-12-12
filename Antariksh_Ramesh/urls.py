d93c6d20683ba6173f5eaeb3bcd44f8e4f4532e0
Antariksh-Ramesh <antariksh.ramesh01@student.csulb.edu>
2023-12-12 00:00:45 -0800

    Create contact

--- Antariksh_Ramesh/urls.py
+++ Antariksh_Ramesh/urls.py
@@ -1,7 +1,11 @@
+# Contact_details/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Contact_details.urls')),
+    path('', RedirectView.as_view(url='contacts/', permanent=False)),
+    path('contacts/', include('Contact_details.urls')),
 ]
--- Contact_details/urls.py
+++ Contact_details/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+# Contact_details/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
 from .views import contact_list, contact_create, contact_update, contact_delete
 
+
 urlpatterns = [
-    path('contacts/', contact_list, name='contact_list'),
-    path('contacts/create/', contact_create, name='contact_create'),
-    path('contacts/<int:pk>/update/', contact_update, name='contact_update'),
-    path('contacts/<int:pk>/delete/', contact_delete, name='contact_delete'),
+    path('', contact_list, name='contact_list'),
+    path('create/', contact_create, name='contact_create'),
+    path('<int:pk>/update/', contact_update, name='contact_update'),
+    path('<int:pk>/delete/', contact_delete, name='contact_delete'),
 ]
